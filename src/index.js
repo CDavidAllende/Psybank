b9b04e5f9d81b986496a39800b1e9937687457c4
lmdc <lmdaviddc@gmail.com>
2025-10-04 19:07:47 -0600

    llevo horas con un error de Header, ya me estreseeee

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 export { default as App } from './App';
+export * from './components/atomos/ContentHeader';
 export * from './components/atomos/icono';
 export * from './components/moleculas/Btnsave';
 export * from './components/organismos/Menuambur';
+export * from './components/organismos/sidebar/Header';
 export * from './components/organismos/sidebar/Sidebar';
 export * from './components/organismos/sidebar/SiderbarCard';
 export * from './components/templates/ConfiguracionTemplate';
